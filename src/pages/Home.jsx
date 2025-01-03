8b047bf0ea94977d82a12d85990b058c8de8a554
hakhan1999 <khanhassaan961@gmail.com>
2025-01-03 16:45:21 +0500

    Island Component Completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const adjustIslanForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
-export default Home
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslanForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'></div> */}
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={0} />
+
+          <Island
+            scale={islandScale}
+            position={islandPosition}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
